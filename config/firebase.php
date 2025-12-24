7c1c689e68f433097fc38ea47174045c246c3b19
Keshav Agarwal <Keshavagarwalbr@gmail.com>
2025-12-24 09:49:02 +0530

    Use Render secret file for Firebase credentials

--- config/firebase.php
+++ config/firebase.php
@@ -2,6 +2,6 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
 use Kreait\Firebase\Factory;
-$factory = (new Factory)->withServiceAccount(__DIR__ . '/firebase-key.json');
+$factory = (new Factory)->withServiceAccount(getenv('GOOGLE_APPLICATION_CREDENTIALS'));
 $firestore = $factory->createFirestore();
 $db = $firestore->database();
